5d3f551ff44a17dab434d4f208bdded4ab8f60db
Attila217 <attila.postafiok@gmail.com>
2020-12-14 15:51:07 +0100

    Class RemoveTask was initialized

---
+++ src/com/company/skills/RemoveTask.java
@@ -0,0 +1,13 @@
+package com.company.skills;
+
+public class RemoveTask extends Skill {
+    public RemoveTask() {
+        command = "-r";
+        description = "Eltávolít egy feladatot";
+    }
+
+    @Override
+    public void action(String[] args) {
+
+    }
+}
