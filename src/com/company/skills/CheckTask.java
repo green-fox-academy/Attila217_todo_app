b29be232827d5eee4578b51ea345f2899b5054aa
Attila217 <attila.postafiok@gmail.com>
2020-12-14 15:51:34 +0100

    Class CheckTask was initialized

---
+++ src/com/company/skills/CheckTask.java
@@ -0,0 +1,13 @@
+package com.company.skills;
+
+public class CheckTask extends Skill {
+    public CheckTask() {
+        command = "-c";
+        description = "Teljesít egy feladatot";
+    }
+
+    @Override
+    public void action(String[] args) {
+
+    }
+}
